86be045c25dc9e68916bda0ef20bf7e5de0d33e4
unknown <prajeed19@gmail.com>
2021-11-29 20:52:07 +0530

    inmakes

---
+++ movieproject/studyapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Movie(models.Model):
+    name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='inmakesproject')
+    desc=models.TextField()
+    year=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
